d42d745f7f84b8e353c2146998bc1038d01ad1b9
Tim Green <rawveg@gmail.com>
2018-02-06 16:55:31 +0000

    Added the carousel widget

---
+++ Block/Widget/Carousel.php
@@ -0,0 +1,11 @@
+<?php
+namespace Pillbox\TrustpilotWidgets\Block\Widget;
+
+use Magento\Framework\View\Element\Template;
+use Magento\Widget\Block\BlockInterface;
+
+class Carousel extends Template implements BlockInterface {
+
+  protected $_template = "widget/carousel.phtml";
+
+}
