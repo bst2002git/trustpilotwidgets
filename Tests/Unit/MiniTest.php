dd71ac3c2653589d9da6732ae0580cc949912418
Tim Green <rawveg@gmail.com>
2018-02-07 09:58:49 +0000

    Added initial test for mini - sample test, dpesn't actually do much

---
+++ Tests/Unit/MiniTest.php
@@ -0,0 +1,11 @@
+<?php
+namespace Pillbox\TrustpilotWidgets\Test\Unit\Mini;
+
+class MiniWidgetTest extends \PHPUnit_Framework_TestCase {
+
+  public function testGetWidget()
+  {
+    $this->assertTrue(false);
+  }
+
+}
