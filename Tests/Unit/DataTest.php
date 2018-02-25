0cd99794888a4be6774b610913c788fa0dbea9c5
Tim Green <rawveg@gmail.com>
2018-02-25 21:55:42 +0000

    Prettyfied the tests a little

--- Tests/Unit/DataTest.php
+++ Tests/Unit/DataTest.php
@@ -16,29 +16,7 @@ class DataTest extends \PHPUnit\Framework\TestCase
 
     // helper_success stores positive (successful) results
     $this->helper = $this->createMock(Data::class);
-    /*$this->helper->method('isEnabled')->willReturn(true);
-    $this->helper->method('getBusinessUnitID')->willReturn('businessunitid');
-    $this->helper->method('getStoreLocale')->willReturn('en_US');
-    $this->helper->method('getConfig')->willReturn(true);*/
 
-  }
-
-  /**
-   * startDisplayInfo
-   * @param  string $method [description]
-   * @param  string $test   [description]
-   */
-  public function startDisplayInfo($method, $test)
-  {
-      fwrite(STDERR, "$method - {$test} running: ");
-  }
-
-  /**
-   * endDisplayInfo
-   */
-  public function endDisplayInfo()
-  {
-      fwrite(STDERR, "\n");
   }
 
   /**
@@ -47,17 +25,11 @@ class DataTest extends \PHPUnit\Framework\TestCase
   public function testIsEnabled()
   {
 
-    // Display test info
-    $this->startDisplayInfo("isEnabled", "testIsEnabled");
-
     // ensure the getConfig method returns the result
     $this->helper->method('getConfig')->willReturn(true);
     $this->helper->method('isEnabled')->willReturn($this->helper->getConfig(true));
 
     $this->assertEquals(true, $this->helper->isEnabled(), 'isEnabled did not return a true value');
-
-    // End Display test info
-    $this->endDisplayInfo();
 
   }
 
@@ -67,17 +39,11 @@ class DataTest extends \PHPUnit\Framework\TestCase
   public function testIsEnabledFalse()
   {
 
-    // Display test info
-    $this->startDisplayInfo("isEnabled", "testIsEnabledFalse");
-
     // ensure the getConfig method returns the result
     $this->helper->method('getConfig')->willReturn(false);
     $this->helper->method('isEnabled')->willReturn($this->helper->getConfig(false));
 
     $this->assertEquals(false, $this->helper->isEnabled(), 'isEnabled did not return a false value');
-
-    // End Display test info
-    $this->endDisplayInfo();
 
   }
 
