02eff94160f8f7b542dc2c2345ea3472e280682a
Tim Green <rawveg@gmail.com>
2018-02-25 22:45:17 +0000

    Simplified tests and added returnValueMap

--- Tests/Unit/DataTest.php
+++ Tests/Unit/DataTest.php
@@ -17,21 +17,15 @@ class DataTest extends \PHPUnit\Framework\TestCase
     // Return value mapping - maps the values that will be returned by the getConfig
     // method, depending on the value passed to it
     $configmap = array(
-      array(
-        'trustpilotwidgets/general/enable',
-        'trustpilotwidgets/general/trustpilot_business_unit_id'
-      ),
-      array(
-        true,
-        '1111111111'
-      )
+      array('trustpilotwidgets/general/enable', true),
+      array('trustpilotwidgets/general/trustpilot_business_unit_id', '1111111111')
     );
 
     // helper_success stores positive (successful) results
     $this->helper = $this->createMock(Data::class);
     $this->helper->method('getConfig')->will($this->returnValueMap($configmap));
-    $this->helper->method('isEnabled')->willReturn($this->helper->getConfig('trustpilotwidgets/general/enable'));
-    $this->helper->method('getBusinessUnitID')->willReturn($this->helper->getConfig('trustpilotwidgets/general/trustpilot_business_unit_id'));
+    //$this->helper->method('isEnabled')->willReturn($this->helper->getConfig('trustpilotwidgets/general/enable'));
+    //$this->helper->method('getBusinessUnitID')->willReturn($this->helper->getConfig('trustpilotwidgets/general/trustpilot_business_unit_id'));
     $this->helper->method('getStoreLocale')->willReturn('en_US');
 
   }
